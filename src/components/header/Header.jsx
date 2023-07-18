96c1bb6b221687fad23e09be3e6f5e7f2484793b
Craig Eaton <craigea92@gmail.com>
2023-07-18 09:52:12 +0100

    Setup the header ready for styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import headerimage from '../../assets/Header_Image.png'
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header>
+  <div className="container header__container">
+    <h5>Hello I'm</h5>
+    <h1>Craig Eaton</h1>
+    <h5 className="text-light">Frontend Developer</h5>
+    <CTA />
+
+    <div className="me">
+      <img src={headerimage} alt=""/>
+    </div>
+
+    <a href="#contact" className="scroll__down">Scroll Down</a>
+
+    <HeaderSocials />
+  </div>
+  </header>;
+  
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return <div className="header__socials">
+    <a href="https://linkedin.com">1</a>
+    <a href="https://github.com">2</a>
+  </div>
+};
+
+export default HeaderSocials;
