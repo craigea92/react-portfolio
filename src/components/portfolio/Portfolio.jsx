89f48207aca53b63659450ec8eaba92aa708e99d
Craig Eaton <craigea92@gmail.com>
2023-07-26 16:33:21 +0100

    added an image and CSS styling to the portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./portfolio.css";
+import aboutimage from "../../assets/About_Image.jpeg";
 
 const Portfolio = () => {
   return (
@@ -8,24 +10,28 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item__image">
+            <img src={aboutimage} alt="" />
+          </div>
           <h3>This is a portfolio title</h3>
-          <a
-            href="https://github.com"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-          <a
-            href="https://.com"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
+          <div className="portfolio__item__cta">
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
         </article>
       </div>
     </section>
