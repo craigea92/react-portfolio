55211db090f56c3509dcf4417aa651afe19d2130
Craig Eaton <craigea92@gmail.com>
2023-07-31 08:35:38 +0100

    Added styling for the contact CTAs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <AiOutlineMail />
+            <AiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>dummyemail@gmail.com</h5>
             <a
@@ -27,40 +27,35 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <AiOutlinePhone />
+            <AiOutlinePhone className="contact__option-icon" />
             <h4>Call</h4>
             <h5>07450252566</h5>
             <a href="+447450252566">Call now</a>
           </article>
           <article className="contact__option">
-            <AiOutlineLinkedin />
+            <AiOutlineLinkedin className="contact__option-icon" />
             <h4>LinkedIn</h4>
             <h5>Craig Eaton</h5>
             <a href="https://www.dummylinkedin.com">Send a message</a>
           </article>
-          <form action="">
-            <input
-              type="text"
-              name="name"
-              placeholder="Your Full Name"
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              required
-            />
-            <textarea
-              name="message"
-              rows="7"
-              placeholder="Your Message required"
-            ></textarea>
-            <button type="submit" className="btn btn-primary">
-              Send Message
-            </button>
-          </form>
         </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message required"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
